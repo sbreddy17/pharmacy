78097065781ab991d67078c4b1ff9440e6b0d89f
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2024-12-12 10:09:35 +0530

    first push

---
+++ src/main/java/com/drucare/DrucareProjectPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.drucare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrucareProjectPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrucareProjectPharmacyApplication.class, args);
+	}
+
+}
